2ada88d3d595dc094e5d6ec5acda85458f49c554
Ziqiang_Cui <33800446+czq1994@users.noreply.github.com>
2019-03-17 14:26:00 +0800

    Update 数值的整数次方.py

--- 数值的整数次方.py
+++ 数值的整数次方.py
@@ -6,9 +6,9 @@ class Solution:
             return 1
         if exponent == 1:
             return base
-        result = self.Power(base, abs(exponent) >> 1)
+        result = self.Power(base, abs(exponent) >> 1) #右移代替除以2
         result *= result
-        if (abs(exponent) & 1 == 1):
+        if (abs(exponent) & 1 == 1): #位与代替判断奇偶
             result = base * result
         if exponent >= 0:
             return result
