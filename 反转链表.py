c727ad0961ed564f354f6db1d8af2aebd4d054eb
Ziqiang_Cui <33800446+czq1994@users.noreply.github.com>
2019-03-17 15:00:53 +0800

    Update 反转链表.py

--- 反转链表.py
+++ 反转链表.py
@@ -3,6 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+# 递归
 class Solution:
     # 返回ListNode
     def ReverseList(self, pHead):
@@ -13,3 +14,18 @@ class Solution:
         pHead.next.next = pHead
         pHead.next = None
         return pNew
+# 非递归
+class Solution:
+    # 返回ListNode
+    def ReverseList(self, pHead):
+        # write code here
+        if not pHead:
+            return None
+        left = None
+        cur = pHead
+        while cur:
+            right = cur.next
+            cur.next = left
+            left = cur
+            cur = right
+        return left
